461a548b9e4762522c2c92bcbaf02f04e0f56fe8
AzkaPrata <azkaprata056@gmail.com>
2022-03-14 13:36:18 +0700

    3

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -2,6 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', express.static('views/index.html'));
+router.use('/', express.static('views/index.html'));
 
 module.exports = router;
